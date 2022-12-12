e2a344b96f3fc0dbf69631501a1b62e353d32b5d
jcheng78 <jcheng78@myseneca.ca>
2022-12-11 19:32:36 -0500

    FINALFINAL

--- validation.js
+++ validation.js
@@ -7,11 +7,11 @@ const error = document.getElementById('error')
 
 
 var checkPostal = /^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$/i;
-console.log("CHECKPOSTAL IS " + checkPostal);
+//console.log("CHECKPOSTAL IS " + checkPostal);
 form.addEventListener('submit', (e) => {
     var valid = checkPostal.test(postal.value);
-    console.log("VALID IS " + valid);
-    console.log("POSTAL VALUE IS " + postal.value);
+    //console.log("VALID IS " + valid);
+    //console.log("POSTAL VALUE IS " + postal.value);
     if (!valid){
         error.innerHTML = "Please enter a valid postal code";
         e.preventDefault();
